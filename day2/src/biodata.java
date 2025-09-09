0375fe6d539a710f16b5f61ab77f38769769cf1f
MUHAMMAD DHIYAUL ATHA <mdhyaulatha@gmail.com>
2025-09-09 12:21:34 +0700

    day 2

---
+++ day2/src/biodata.java
@@ -0,0 +1,29 @@
+public class biodata {
+    public static void main(String[] args) {
+        
+        String nama = "MUHAMMAD DHIYAUL ATHA";
+        String Alamat = "Punteut";
+        String tempatLahir = "Meulaboh";
+        String hobi = "olahraga";
+        String warnaKesukaan = "coklat";
+        int nilai = 85;
+        float ipk = 3.6f;
+        boolean logika = true;
+        char karakter = 'A';
+        String tanggalLahir = "20 Februari 2007";
+
+        System.out.println("NAMA            : " + nama);
+        System.out.println("ALAMAT          : " + Alamat);
+        System.out.println("TEMPAT LAHIR    : " + tempatLahir);
+        System.out.println("TANGGAL LAHIR   : " + tanggalLahir);
+        System.out.println("HOBI            : " + hobi);
+        System.out.println("WARNA KESUKAAN  : " + warnaKesukaan);
+
+        System.out.println("______");
+
+        System.out.println("isi variabel nilai      = " + nilai);
+        System.out.println("isi variabel IPK        = " + ipk);
+        System.out.println("isi variabel logika     = " + logika);
+        System.out.println("isi variabel karakter   = " + karakter);
+    }
+}
---
+++ day2/src/percobaan.java
@@ -0,0 +1,14 @@
+public  class percobaan {
+    public static void main(String[] args) {
+        
+        String nama = "MUHAMMAD DHIYAUL ATHA";
+        int umur = 18;
+        double tinggi = 150.555;
+        float berat = 50.5f;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Berat: " + berat);
+    }
+}
