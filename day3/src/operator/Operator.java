1df610fb75afae599f8a03f14724a7b5679c0697
MUHAMMAD DHIYAUL ATHA <mdhyaulatha@gmail.com>
2025-09-16 11:32:59 +0700

    day 3

---
+++ day3/src/operator/Operator.java
@@ -0,0 +1,28 @@
+
+
+public class Operator {
+    /* operator matematika
+     * - + / %
+     * 
+     * operator assigment
+     * = += -= /= *= &=
+     * a += b;
+     * 
+     * operator increment
+     * (post increment a++) (pre increment ++a)
+     * operator decrement
+     * (post decrement a--) (pre decrement a--)
+     * 
+     * operator pembanding
+     * ++
+     * !=
+     * >
+     * <
+     * >=
+     * <=
+     */
+
+     public static void main(String[] args) {
+         
+     }
+}
