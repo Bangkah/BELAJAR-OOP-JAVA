97bc028849c756676dc5ca86acad0be4869630d5
MUHAMMAD DHIYAUL ATHA <mdhyaulatha@gmail.com>
2025-09-16 11:35:28 +0700

    day 3

--- day3/src/operator/Operator.java
+++ day3/src/operator/Operator.java
@@ -23,6 +23,10 @@ public class Operator {
      */
 
      public static void main(String[] args) {
-         
+         int a,b;
+         a = 10;
+         b = 20;
+
+         System.out.println("a = " + a);
      }
 }
